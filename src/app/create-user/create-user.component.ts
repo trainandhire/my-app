e56194d65c664a25ddafccd5451e48c808594f6e
trainandhire <projects.trainandhire@gmail.com>
2022-11-01 10:26:29 +0530

    Validations implemented

--- src/app/create-user/create-user.component.ts
+++ src/app/create-user/create-user.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormArray, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-user',
@@ -10,17 +10,41 @@ export class CreateUserComponent implements OnInit {
 
   public userForm: FormGroup = new FormGroup(
     {
-      name: new FormControl(),
-      phone: new FormControl(),
+      name: new FormControl(null,[Validators.required, Validators.minLength(3), Validators.maxLength(10)]),
+      phone: new FormControl(null,[Validators.required, Validators.min(1000000000), Validators.max(9999999999)]),
       dob: new FormControl(),
-      adderss: new FormGroup(
+      address: new FormGroup(
         {
           city: new FormControl(),
           pin: new FormControl()
         }
-      )
+      ),
+      cards: new FormArray([]),
+      type: new FormControl(),
+      busFee: new FormControl(),
+      hostelFee: new FormControl()
     }
   )
+
+  get cardsFormArray(){
+    return this.userForm.get('cards') as FormArray;
+  }
+
+  add(){
+    this.cardsFormArray.push(
+      new FormGroup(
+        {
+          name: new FormControl(),
+          expiry: new FormControl(),
+          cvv: new FormControl()
+        }
+      )
+    )
+  }
+
+  delete(i:number){
+    this.cardsFormArray.removeAt(i);
+  }
 
   constructor() { }
 
